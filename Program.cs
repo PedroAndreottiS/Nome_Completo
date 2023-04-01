7452857e6d688fa2031f63943d093e697a89a0a0
Pedro Andreotti Sales <Pedroandreottijetforce@gmail.com>
2023-03-31 21:25:45 -0300

    Atividade concluida, habilidades aprimoradas

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite seu nome: ");
+    string nome =  Console.ReadLine()!;
+    Console.WriteLine("");
+Console.Write("Digite seu sobrenome:");
+    string sobrenome = Console.ReadLine()!;
+      Console.WriteLine("");
+
+
+Console.Write("Nome completo: ");
+Console.WriteLine(nome + " " + sobrenome);
+  Console.WriteLine("");
+ Console.Write("Nome completo em catálogo: ");
+ Console.WriteLine($"{sobrenome.ToUpper()},{nome}{"."}");
